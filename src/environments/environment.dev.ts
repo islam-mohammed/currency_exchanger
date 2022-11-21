a561efffff999f4c08f959680620d9cd666bfda4
Islam Mohammed <islammohammed1978@gmail.com>
2022-11-22 00:59:13 +0200

    fix desing issue and add new key for testing

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   name: 'dev',
-  api_key: 'SrSNe1ShtZReg1n6uOhPaGs0BfvTQV7w',
+  api_key: 'xlwaxFaFldSAoNkKDVCCZ7GMQFB0JrT7',
   api_url: 'https://api.apilayer.com/currency_data',
   most_popular_currencies: 'USD,EUR,JPY,GBP,AUD,CAD,CHF,CNY,HKD,NZD',
 };
